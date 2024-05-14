f816c0ab5e17852cc0f285cb3c7abcd6983384d7
Marcello Vartuli <mvartuli@gmail.com>
2024-05-14 20:00:40 -0300

    deploy inicial do app

---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pickle
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.ticker import MaxNLocator
+
+st.markdown("# Modelo preditivo do petróleo brent !")
+
+with open('modelo_brent.pkl', 'rb') as file_2:
+    modelo_brent = pickle.load(file_2)
+
+
+# Carregar o DataFrame
+df = pd.read_csv('ipea.csv')
+df['Data'] = pd.to_datetime(df['Data'])
+df = df.sort_values(by='Data', ascending=True).reset_index(drop=True)
+
+st.dataframe(df)
+
+#df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
+#df['Preço'] = df['Preço'].astype(float)  # Certifique-se de que os preços são float
+
+# É uma boa prática criar recursos de atraso (lag features) para séries temporais
+# Vamos criar alguns para nosso modelo
+# Criar recursos de atraso (lag features)
+lags = 7
+for lag in range(1, lags + 1):
+    df[f'Preço_lag_{lag}'] = df['Preço - petróleo bruto - Brent (FOB)'].shift(lag)
+
+# Removemos quaisquer linhas com valores NaN que foram criados ao fazer o shift
+df = df.dropna()
+
+# Preparando os dados para treinamento
+X = df[['Preço_lag_1', 'Preço_lag_2']].values  # Inputs são os preços atrasados
+y = df['Preço - petróleo bruto - Brent (FOB)'].values  # Output é o preço atual
+
+# Dividir os dados em conjuntos de treino e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=False)
+
+# Fazer previsões
+predictions = modelo_brent.predict(X_test)
+
+# Avaliar o modelo
+mse = mean_squared_error(y_test, predictions)
+mae = mean_absolute_error(y_test, predictions)
+
+st.markdown(f'### O Erro quadrado médio é de: {mse:.2f}')
+st.markdown(f'### O Erro absoludo médio é de: {mae:.2f}')
+
+# Fazer previsões para a próxima semana usando os últimos dados conhecidos
+last_known_data = X[-1].reshape(1, -1)
+next_week_predictions = []
+for _ in range(7):  # para cada dia da próxima semana
+    next_day_pred = modelo_brent.predict(last_known_data)[0]
+    next_week_predictions.append(next_day_pred)
+    last_known_data = np.roll(last_known_data, -1)
+    last_known_data[0, -1] = next_day_pred
+
+# As datas correspondentes à próxima semana
+next_week_dates = pd.date_range(df['Data'].iloc[-1], periods=8)[1:]
+
+# Selecionar os dados da semana atual (últimos 7 dias do dataset)
+current_week_dates = df['Data'].iloc[-7:]
+current_week_prices = df['Preço - petróleo bruto - Brent (FOB)'].iloc[-7:]
+
+for week, pred in zip(next_week_dates, next_week_predictions):
+    print(f'{week}: {pred:.2f}')
+
+# Plotar os preços reais da semana atual e as previsões para a próxima semana
+
+plt.figure(figsize=(10, 5))
+plt.plot(current_week_dates, current_week_prices, 'bo-', label='Preços Atuais')
+plt.plot(next_week_dates, next_week_predictions, 'r--o', label='Previsões para a Próxima Semana')
+
+# Formatar o eixo x para exibir datas
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.gcf().autofmt_xdate()  # Ajustar formato das datas para evitar sobreposição
+
+plt.xlabel('Data')
+plt.ylabel('Preço')
+plt.title('Preços Reais e Previsões para as Últimas Duas Semanas')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+st.pyplot(plt)
+
+## parei no 1:23 da live
