99269921c68726a65b509b7aa0e541b1a5f6912b
Marcello Vartuli <mvartuli@gmail.com>
2024-05-14 20:42:32 -0300

    correção no arquivo requirements

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import joblib
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split
 import numpy as np
